9d5b363a3682d1a8200bde10faba67aa0de0836f
bhanu3271 <bhanuprakash7013403006@gmail.com>
2026-07-15 18:50:25 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,9 @@
 const SUPABASE_URL = "https://ntdchtznalgitinaorrn.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_publishable_l2GmHsTFESBJjqV8FOAt_w_o1qLlmoV";
+"sb_publishable_l2GmHsTFESBJjqV8FOAt_w_o1qLlmoV";
 
 const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+    SUPABASE_URL,
+    SUPABASE_KEY
 );

7e8c9735070072825058221f5768b6543c07d53b
bhanu3271 <bhanuprakash7013403006@gmail.com>
2026-07-15 18:13:56 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://ntdchtznalgitinaorrn.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_secret_W35IphUwBqOGFvFPADENPg_nZACImr3";
+  "sb_publishable_l2GmHsTFESBJjqV8FOAt_w_o1qLlmoV";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
